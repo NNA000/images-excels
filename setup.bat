9330a9c0781bfb13af8d1ddd3473fe4fa8f980b2
Apfata <apfata.developer@gmail.com>
2023-03-15 11:53:58 -0500

    Arquitectura proyecto p2

---
+++ setup.bat
@@ -0,0 +1,24 @@
+@echo off
+if "%1" neq "" (
+    if "%1" equ "install" (
+        echo "Installing env..."
+        REM create env --> activate env ---> install requirement
+        call conda create --name images-excels python==3.9.0 anaconda -y
+        call conda activate images-excels
+        call pip install -r requirement.txt
+    ) else if "%1" equ "ur" (
+        echo "Updating requirement..."
+        REM Just for update requirement
+        call pip install -r requirement.txt
+    ) else (
+        if "%1" equ "run" (
+            call python run.py
+        ) else (
+            echo "No one... %0 %1"
+        )
+    )
+) else (
+    REM Case when just want to activate environment
+    echo "Activating env, to deactivate conda deactivate..."
+    call conda activate images-excels
+)
